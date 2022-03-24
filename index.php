c5b08126e14f7321de8cecfc4d6e5b94c21d5576
cannawarp <100110974+cannawarp@users.noreply.github.com>
2022-03-23 20:34:22 -1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>WEBPAGE</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    </header>
+    <body>
+    <div class="main">
+        <div id="wrapper">
+            <div id="logo">
+            <img src="Logo.png">
+            </div>
+            <div id="navigation">
+            <ul>
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="action.html">Actions</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="aboutus.html">About Us</a></li>
+            </ul>
+            </div>
+            <div id="title" style="background-image:url(bg-image2.jpg); background-size: cover">
+                <h1>POVERTY & DEVELOPMENT</h1>
+                <h2>In the Philippines</h2>
+            </div>
+            <div id="content">
+            <h2>Poverty in the Philippines</h2>
+            <p>The Philippines has a fairly high poverty rate with more than 16% of the population living below the poverty line. Because of the many people reliant on agriculture for an income and inequality in wealth distribution, about 17.6 million Filipinos struggle to afford basic necessities. From 2015 to 2020, the rate of poverty declined from 21.6% to 16.6%. Philippine President Rodrigo Duterte aims to reduce the rate of poverty to 14% by 2022. Through its strategy, AmBisyon 2040, the Philippine government plans to eradicate extreme poverty by 2040. Furthermore, the government has implemented various programs and reforms to reduce poverty by targeting education, healthcare and the overall economy. Here are five ways the program is combating poverty in the Philippines.</p>
+            <p>Although the island’s poverty rate has recently fallen from 26.6 percent in 2006 to 21.6 percent in 2015, it still saw approximately 22 million living below the national poverty line. That is over one-fifth of the population.</p>
+            <p>The creation of jobs outside of agriculture is helping lift the nation out of impoverishment. Unfortunately, constraints like inequality of income and opportunities, the effects of natural disasters and an increasing population prevent many families from achieving a higher quality of life.</p>
+            <p>Homelessness is something that many Filipino citizens contend with. The Philippines has a rapidly increasing population. In fact, it is estimated to reach 12 million by 2030. Currently, 44 percent of people residing in urban environments live in slums. Furthermore, 1.2 million children are homeless throughout the islands. Manila, the capital, holds 3.1 million homeless Filipinos. Of these residents, 70,000 are children. An imminent need for affordable and durable houses is upon the nation.</p>
+            <p>The invention of bamboo houses is an innovative solution to finally aid this country’s poverty and homeless crisis.</p>
+            </div>
+            <footer id="footer">
+                <p style="font-family: sans-serif">Copyright 2022 creative coding. All Right Reserved</p>
+            </footer>
+        </div>
+    </div>
+    </body>
+</html>
